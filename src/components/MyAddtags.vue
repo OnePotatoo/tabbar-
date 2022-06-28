f5264e6dda4829e062ce89a80f25d713f79daf93
yyh <958182321@qq.com>
2022-06-28 16:35:49 +0800

    完成了案例-tabbar

---
+++ src/components/MyAddtags.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <button
+      class="btn btn-primary btn-sm"
+      v-if="isShow"
+      @dblclick="isShow = false"
+    >
+      +Tag
+    </button>
+    <input
+      v-else
+      type="text"
+      style="width: 80px"
+      v-model="tagname"
+      v-focus
+      @blur="addTags(tagname)"
+      @keydown.enter="addTags"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+      tagname: ''
+    }
+  },
+  methods: {
+    addTags() {
+      this.isShow = true
+      if (this.tagname !== '') {
+        this.$emit('add-tags', this.tagname)
+      }
+      this.tagname = ''
+    }
+  },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus()
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
